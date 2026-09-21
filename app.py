666058459858f90baac8e50dadf14d4d4ec36e6a
Mayuri Singh <mayurisingh960@gmail.com>
2026-09-22 01:08:07 +0530

    Template integration

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    project_name = "DeepFakeChain"
+
+    return render_template(
+        "index.html",
+        project=project_name
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
