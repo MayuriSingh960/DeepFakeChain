3b6ee0ec65142074572492f07561b2466376451a
Mayuri Singh <mayurisingh960@gmail.com>
2026-09-22 18:36:30 +0530

    Added image upload functionality

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+UPLOAD_FOLDER = "uploads"
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -8,16 +11,39 @@ def home():
     print("Request received:", request.method)
 
     if request.method == "POST":
+
         print("POST detected")
 
-        image = request.files["image"]
+        if "image" in request.files:
 
-        print("Filename:", image.filename)
+            image = request.files["image"]
+
+            if image.filename == "":
+
+                print("No file selected")
+
+            else:
+
+                print("Filename:", image.filename)
+
+                file_path = os.path.join(
+                    UPLOAD_FOLDER,
+                    image.filename
+                )
+
+                image.save(file_path)
+
+                print("Image saved successfully")
+
+        else:
+
+            print("No image uploaded")
 
     return render_template(
         "index.html",
         project="DeepFakeChain"
     )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
