40db11dc2e24f00046ea55e45b799e9bd10af7be
Mayuri Singh <mayurisingh960@gmail.com>
2026-09-23 21:53:30 +0530

    Added image file validation

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 UPLOAD_FOLDER = "uploads"
+
+ALLOWED_EXTENSIONS = {"jpg", "jpeg", "png"}
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -26,14 +29,22 @@ def home():
 
                 print("Filename:", image.filename)
 
-                file_path = os.path.join(
-                    UPLOAD_FOLDER,
-                    image.filename
-                )
+                extension = image.filename.rsplit(".", 1)[1].lower()
 
-                image.save(file_path)
+                if extension in ALLOWED_EXTENSIONS:
 
-                print("Image saved successfully")
+                    file_path = os.path.join(
+                        UPLOAD_FOLDER,
+                        image.filename
+                    )
+
+                    image.save(file_path)
+
+                    print("Image saved successfully")
+
+                else:
+
+                    print("Only JPG, JPEG and PNG files are allowed")
 
         else:
 
